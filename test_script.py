5ca3072d2a8fe5c91ecf3dffa99dd0f3bf24a4fb
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2021-11-15 00:07:29 +0530

    add some basic tests

---
+++ test_script.py
@@ -0,0 +1,34 @@
+import ipaddress
+
+from dj_server_ip import (get_host_ip, get_local_ip, get_public_ip,
+                          get_socket_ip, server_ip)
+
+
+def test_public_ip():
+    ip = get_public_ip()
+    ip = ipaddress.IPv4Address(ip)
+    assert ip.is_private is False
+
+
+def test_socket_ip():
+    ip = get_socket_ip()
+    ip = ipaddress.IPv4Address(ip)
+    assert ip.is_private is True
+
+
+def test_local_ip():
+    ips = get_local_ip()
+    ips = [ipaddress.IPv4Address(item) for item in ips]
+    assert all(ips) is True
+
+
+def test_host_ip():
+    ips = get_host_ip()
+    ips = [ipaddress.IPv4Address(item) for item in ips]
+    assert all(ips) is True
+
+
+def test_server_ip():
+    ips = server_ip()
+    ips = [ipaddress.IPv4Address(item) for item in ips]
+    assert all(ips) is True
